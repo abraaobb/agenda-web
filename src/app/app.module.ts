8c4a16c30daf694e8316301c76abf19a59b92349
abraao brandao <abraao.brandao@fpf.br>
2022-03-04 14:54:00 -0400

    using angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ContactComponent } from './components/contact/contact.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ContactComponent} from './components/contact/contact.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { ConfigService } from './services/config.service';
-import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {ConfigService} from './services/config.service';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from "./modules/material.module";
+
 @NgModule({
-    declarations: [AppComponent, ContactComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-    providers: [ConfigService, HttpClientModule],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent, ContactComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, BrowserAnimationsModule, MaterialModule],
+  providers: [ConfigService, HttpClientModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
